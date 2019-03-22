493979b8d64162809f0d1a8c3e74f5c54f08324e
ThearyIm <thearyim@gmail.com>
2019-03-22 12:41:13 -0700

    update js

--- src/js/ageCalculator.js
+++ src/js/ageCalculator.js
@@ -2,14 +2,39 @@
 export default class AgeCalculator {
   constructor() {
     this.age = 0;
-    this.planets = [earth, mercury, venus, mars, jupiter];
+    this.planets = ["earth", "mercury", "venus", "mars", "jupiter"];
     this.ageFactors = [1, .24, .62, 1.88, 11.86];
     this.lifeExpectancy = [78, 18.72, 48.36, 146.64, 925.08];
     this.lifeStyleFactors = ["smoke", "drink", "exercise"];
   }
-
-  getAge(planet, age) {
-    let age = new Age();
-    this.planets.ageFactors();
-  }
 }
+
+AgeCalculator.prototype.getAge = function () {
+  let calculator = document.getAgebyPlanet('planet', 'age');
+
+  for (let currentPlanet = 0; currentPlanet < planets.lenght; currentPlanet++) {
+    let planet = calculator.planets[currentPlanet];
+
+    for (let currentAgeFactor = 0; currentAgeFactor < ageFactors.length; currentAgeFactor++) {
+      let ageFactor = calculator.ageFactors[currentAgeFactor];
+      while (age !=0) {
+        calculator[currentPlanet] = planets[currentPlanet].ageFactors[currentAgeFactor];
+      }
+
+    
+    }
+  }
+};
+
+  getAge('planet', 'age') {
+
+
+    this.planets.ageFactors();
+
+    for (var i = 0; i <= planets.length; i++)
+    let planet = calculator.planets[planetName];
+  }
+
+  getLifeExpectany(lifeStyleFactors, planet) {
+
+  }
